41e497cd46bfdc38ef5290c664a01e1f62170562
John Doe <johndoe@example.com>
2025-08-20 10:49:09 -0500

    t

---
+++ Lincoln.java
@@ -0,0 +1,9 @@
+public class Lincoln
+{
+    public static void main(String[] args)
+    {
+        System.out.println("A quote by Abraham Lincoln:");
+
+        System.out.println("A quote by Abraham Lincoln:");
+    }
+}
