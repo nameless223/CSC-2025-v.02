7b3954c2c39006dc26ce122db3fe39fa6ab89eb2
nameless223 <69223252+nameless223@users.noreply.github.com>
2025-08-25 18:46:52 -0500

    Update Lincoln.java

--- Lincoln.java
+++ Lincoln.java
@@ -4,6 +4,6 @@ public class Lincoln
     {
         System.out.println("A quote by Abraham Lincoln:");
 
-        System.out.println("A quote by Abraham Lincoln:");
+        System.out.println("Character is like a tree, and reputation like its shadow. The shadow is what we think of it; the tree is the real thing.");
     }
 }
