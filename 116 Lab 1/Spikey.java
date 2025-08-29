5e8909d17abad01eb24bfd1bce43934619a9d193
John Doe <johndoe@example.com>
2025-08-29 10:12:19 -0500

    update

---
+++ 116 Lab 1/Spikey.java
@@ -0,0 +1,13 @@
+public class Spikey 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+    }
+}
+
