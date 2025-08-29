8f7d316171f84fe0be322ccc8bb62bc9f2eb2809
John Doe <johndoe@example.com>
2025-08-29 10:19:22 -0500

    update

--- 116 Lab 1/Tricky.java
+++ 116 Lab 1/Tricky.java
@@ -2,10 +2,10 @@ public class Tricky
 {
     public static void main(String[] args) 
     {
-    System.out.println("Hello world");
-    System.out.println("Do you like this program?");
-    System.out.println();
+        System.out.println("Hello world");
+        System.out.println("Do you like this program?");
+        System.out.println();
 
-    System.out.println("I wrote it myself.");
+        System.out.println("I wrote it myself.");
     }
 }
